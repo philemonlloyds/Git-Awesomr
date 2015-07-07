f6f60d5f1b4c65fbba63c539c96864a102495438
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-07-07 19:17:59 +0000

    URL CHange

--- app/actions.rb
+++ app/actions.rb
@@ -284,7 +284,7 @@ get '/user/:username' do
   erb :'user/index'
 end
 
-get 'http://gitawesomer.herokuapp.com/login' do
+get 'gitawesomer.herokuapp.com/login' do
   if !authenticated?
     authenticate!
   else
@@ -313,7 +313,7 @@ get 'http://gitawesomer.herokuapp.com/login' do
 end
 end
 
-get 'http://gitawesomer.herokuapp.com/callback' do
+get 'gitawesomer.herokuapp.com/callback' do
   session_code = request.env['rack.request.query_hash']['code']
   result = Octokit.exchange_code_for_token(session_code, CLIENT_ID, CLIENT_SECRET)
   session[:access_token] = result[:access_token]
@@ -359,16 +359,16 @@ get 'http://gitawesomer.herokuapp.com/callback' do
   redirect "/user/#{user.username}"
 end
 
-get 'http://gitawesomer.herokuapp.com/logout' do
+get 'gitawesomer.herokuapp.com/logout' do
   session.clear
   redirect '/'
 end
 
-get 'http://gitawesomer.herokuapp.com/user' do
+get 'gitawesomer.herokuapp.com/user' do
   erb :'user/index'
 end
 
-get 'http://gitawesomer.herokuapp.com/id' do
+get 'gitawesomer.herokuapp.com/id' do
   @user = current_user
   erb :'/users/id'
 end
@@ -390,7 +390,7 @@ end
 #     end
 # end
 
-get 'http://gitawesomer.herokuapp.com/group' do
+get 'gitawesomer.herokuapp.com/group' do
   # GRAPH DATA
   @user_id = params["id"]
   @user = User.find(@user_id)
